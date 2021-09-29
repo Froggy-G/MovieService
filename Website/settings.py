67d3c1c8801cf0a4dab4727beae0086b6d869103
Froggy-G <pwe756@yandex.ru>
2021-09-29 20:32:45 +0400

    change bd to PostgreSQL

--- Website/settings.py
+++ Website/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = "Website.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "Movie_database",
+        'USER': 'postgres',
+        'PASSWORD': 'aezakmi322',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
